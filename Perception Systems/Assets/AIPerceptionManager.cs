6cd59e96d07f3e39c6af7387c84ecb0fadafac7d
Sandra Alvarez <sandruskiag@gmail.com>
2018-10-22 17:02:19 +0200

    Perception Systems

---
+++ Perception Systems/Assets/AIPerceptionManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PerceptionEvent
+{
+	public enum senses { VISION, SOUND };
+	public enum types { NEW, LOST };
+
+	public GameObject go;
+	public senses sense;
+	public types type;
+}
+
+public class AIPerceptionManager : MonoBehaviour {
+
+	public GameObject Alert;
+
+	// Update is called once per frame
+	void PerceptionEvent (PerceptionEvent ev) {
+
+		if(ev.type == global::PerceptionEvent.types.NEW)
+		{
+			Debug.Log("Saw something NEW: " + ev.go.name);
+			Alert.SetActive(true);
+		}
+		else
+		{
+			Debug.Log("LOST: " + ev.go.name);
+			Alert.SetActive(false);
+		}
+	}
+}
---
+++ Perception Systems/Assets/Steering/SteeringFollowPath.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using BansheeGz.BGSpline.Components;
+using BansheeGz.BGSpline.Curve;
+
+public class SteeringFollowPath : SteeringAbstract {
+
+	public BGCcMath path;
+	public float accuracy = 1.0f;
+
+	float current_percentage = 0.0f;
+	float distance_ratio = 0.1f;
+	Move move;
+	SteeringSeek seek;
+
+	// Use this for initialization
+	void Start () {
+		move = GetComponent<Move>();
+		seek = GetComponent<SteeringSeek>();
+
+		// TODO 1: Calculate the closest point in the range [0,1] from this gameobject to the path
+		path.CalcPositionByClosestPoint(transform.position, out current_percentage);
+		distance_ratio = move.max_mov_velocity / path.GetDistance();
+		current_percentage /= path.GetDistance();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(path != null)
+		{
+			Vector3 target = Vector3.zero;
+
+			// TODO 2: Check if the tank is close enough to the desired point
+			// If so, create a new point further ahead in the path
+			target = path.CalcPositionByDistanceRatio(current_percentage);
+
+			//float path_len = path.length;
+			float distance = (target - transform.position).magnitude;
+
+			if(distance < accuracy)
+			{
+				current_percentage += distance_ratio;
+				if(current_percentage > 1.0f)
+					current_percentage -= 1.0f;
+			}
+
+			seek.Steer(target, priority);
+		}
+	}
+
+	void OnDrawGizmosSelected() 
+	{
+
+		if(isActiveAndEnabled)
+		{
+			// Display the explosion radius when selected
+			Gizmos.color = Color.green;
+			Gizmos.DrawWireSphere(path.CalcPositionByClosestPoint(transform.position), accuracy);
+		}
+
+	}
+}
